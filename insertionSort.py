705e8b74072bbebeeb9f25d2db1e98c46a23942d
qiyue0421 <qiyue0421@gmail.com>
2019-04-11 10:34:00 +0800

    插入排序的实现

---
+++ insertionSort.py
@@ -0,0 +1,13 @@
+def insertionSort(alist):
+    # 需要遍历n-1次
+    for index in range(1, len(alist)):
+        # 将比较项保存
+        currentvalue = alist[index]
+        pos = index
+        # 子列表的项大于比较项
+        while pos > 0 and alist[pos-1] > currentvalue:
+            # 向右移位子列表
+            alist[pos] = alist[pos-1]
+            pos -= 1
+        # 将比较项插入到正确位置
+        alist[pos] = currentvalue
