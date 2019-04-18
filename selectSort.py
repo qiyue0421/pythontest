5dc20737bc492386bf5d2f35b240e83307f81e13
qiyue0421 <qiyue0421@gmail.com>
2019-04-18 16:46:55 +0800

    添加注释

--- selectSort.py
+++ selectSort.py
@@ -3,7 +3,7 @@ def selectSort(alist):
     # 遍历n-1次排序
     for fillsolt in range(len(alist)-1, 0, -1):
         position_max = 0
-        # 遍历列表，这里从1开始的原因是，第一个元素赋值给了position_max
+        # 遍历列表，找出最大值的索引。这里从1开始是因为第一个元素赋值给了position_max
         for location in range(1, fillsolt+1):
             if alist[location] > alist[position_max]:
                 # 总是将最大值存入position_max中
